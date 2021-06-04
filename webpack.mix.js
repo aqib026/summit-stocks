e2d051c5b1a075bd38f75c7b670c940b5a5542b4
[aqibtosol@gmail.com] <[aqibtosol@gmail.com]>
2021-06-04 13:46:47 +0500

    changes

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'js')
     .vue()
-    .postCss('resources/css/app.css', 'public/css', [
+    .postCss('resources/css/app.css', 'css', [
         require('postcss-import'),
         require('tailwindcss'),
         require('autoprefixer'),
